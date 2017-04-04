a3ba525b59cbe6c5807db1670f8a5d63eb69f25d
James Tapsell <email@jrtapsell.co.uk>
2017-04-04 16:28:53 +0100

    Initial commit

---
+++ static/js/firebase_init.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyD-b_XD6-Eoe-hQnSsIyHt_s2P2bexLu_E",
+  authDomain: "jrtapsell-ctf.firebaseapp.com",
+  databaseURL: "https://jrtapsell-ctf.firebaseio.com",
+  projectId: "jrtapsell-ctf",
+  storageBucket: "jrtapsell-ctf.appspot.com",
+  messagingSenderId: "706159874560"
+};
+firebase.initializeApp(config);
---
+++ static/js/index.js
@@ -0,0 +1,27 @@
+(function() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      var database = firebase.database();
+      database.ref("/users/" + user.uid).set({
+        "uid": user.uid,
+        "name": user.displayName,
+        "last_login": Date()
+      });
+      $.get('static/templates/home.html', function (data) {
+        var template=Handlebars.compile(data);
+        $("#page-content").html(template({}));
+      }, 'html');
+      /*
+      database.ref("/users/").on("value", function (data) {
+        var stringify = JSON.stringify(data.val(), null, 4);
+        var users = $("#users");
+        users.text(stringify);
+      });
+      */
+    } else {
+      alert("Please log in");
+      firebase.auth().signInWithPopup(provider);
+    }
+  });
+})();
