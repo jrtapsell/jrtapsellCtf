4bf3f8cebc114b3e600a2c34ea245eca3010ba43
James Tapsell <email@jrtapsell.co.uk>
2017-04-06 20:21:16 +0100

    Trying to add user recorder

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,12 @@
   var unsubscribe = firebase.auth().onAuthStateChanged(function (user) {
     unsubscribe();
       if (user) {
+        console.log(user);
+        /*
+        firebase.database().ref("/users/").set({
+
+        });
+        */
         redirect_to_url();
       } else {
         load_login();
