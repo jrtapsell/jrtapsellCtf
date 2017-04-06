6259bfe4db4cef4fdd9388674bba6e4a3c816bd9
James Tapsell <email@jrtapsell.co.uk>
2017-04-06 16:10:17 +0100

    Trying detatch code

--- static/js/index.js
+++ static/js/index.js
@@ -1,36 +1,11 @@
 (function() {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  var first = true;
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (first) {
-      first = false;
+  var unsubscribe = firebase.auth().onAuthStateChanged(function (user) {
+    unsubscribe();
       if (user) {
-        /*
-        $(".mdl-layout__drawer-button").show();
-        firebase.database().ref("/users/" + user.uid).set({
-          "uid": user.uid,
-          "name": user.displayName,
-          "image": user.photoURL
-        });
-        load_index();
-
-        var myConnectionsRef = firebase.database().ref("/users/" + user.uid + '/connections');
-        var lastOnlineRef = firebase.database().ref("/users/" + user.uid + '/lastOnline');
-
-        var connectedRef = firebase.database().ref('.info/connected');
-        connectedRef.on('value', function(snap) {
-          if (snap.val() === true) {
-            var con = myConnectionsRef.push(true);
-            con.onDisconnect().remove();
-            lastOnlineRef.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP);
-          }
-        });
-        */
         redirect_to_url();
       } else {
         $(".mdl-layout__drawer-button").hide();
         load_login();
       }
-    }
   });
 })();
