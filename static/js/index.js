46f0a89f2e2b626e92008f307d9a514da55ce358
James Tapsell <email@jrtapsell.co.uk>
2017-04-06 13:04:38 +0100

    Moved connection status to all pages

--- static/js/index.js
+++ static/js/index.js
@@ -5,8 +5,28 @@
     if (first) {
       first = false;
       if (user) {
+        $(".mdl-layout__drawer-button").show();
+        firebase.database().ref("/users/" + user.uid).set({
+          "uid": user.uid,
+          "name": user.displayName,
+          "image": user.photoURL
+        });
+        load_index();
+        
+        var myConnectionsRef = firebase.database().ref("/users/" + user.uid + '/connections');
+        var lastOnlineRef = firebase.database().ref("/users/" + user.uid + '/lastOnline');
+
+        var connectedRef = firebase.database().ref('.info/connected');
+        connectedRef.on('value', function(snap) {
+          if (snap.val() === true) {
+            var con = myConnectionsRef.push(true);
+            con.onDisconnect().remove();
+            lastOnlineRef.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP);
+          }
+        });
         redirect_to_url();
       } else {
+        $(".mdl-layout__drawer-button").hide();
         load_login();
       }
     }
