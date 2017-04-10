ca0f747957f6b589116cd3c518f2ba2c2557c123
James Tapsell <email@jrtapsell.co.uk>
2017-04-10 12:22:22 +0100

    Moved to gulp deploy

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var header = require('gulp-header');
+
+const deploy = "deploy/static/";
+
+gulp.task('template', function() {
+  return gulp.src('src/templates/*.html')
+    .pipe(header('<!--TEMPLATE-->\n'))
+    .pipe(gulp.dest(deploy + 'templates'))
+});
+
+gulp.task('html', function(){
+  return gulp.src('src/html/*.html')
+    .pipe(gulp.dest(deploy + 'html'))
+});
+
+gulp.task('js', function(){
+  return gulp.src('src/js/*.js')
+    .pipe(gulp.dest(deploy + 'js'))
+});
+
+gulp.task('default', [ 'html', 'js', 'template' ]);
