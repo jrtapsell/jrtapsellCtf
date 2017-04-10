6872d28dd8538d57c300168207bad60cd157c115
James Tapsell <email@jrtapsell.co.uk>
2017-04-10 11:12:14 +0100

    Logging URLs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 this.addEventListener('fetch', function(event) {
-  console.log("SERVICE WORKER:", event);
+  console.log("SERVICE WORKER:", event, event.request.url);
   event.respondWith(
     fetch(event.request)
   );
