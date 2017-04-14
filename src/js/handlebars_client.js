522c86068cec665135cfa11e3ad637cfe44295e1
James Tapsell <email@jrtapsell.co.uk>
2017-04-14 15:27:28 +0100

    Added reverseEach to fix messages being unreadable

--- src/js/handlebars_client.js
+++ src/js/handlebars_client.js
@@ -8,3 +8,18 @@ Handlebars.registerHelper("userIcon", function (userId) {
   const escaped_id = Handlebars.Utils.escapeExpression(userId);
   return new Handlebars.SafeString("<div class='user-icon' data-id='" + userId + "'></div>");
 });
+
+Handlebars.registerHelper("reverseEach", function (context) {
+  var options = arguments[arguments.length - 1];
+  var ret = '';
+
+  if (context && context.length > 0) {
+    for (var i = context.length - 1; i >= 0; i--) {
+      ret += options.fn(context[i]);
+    }
+  } else {
+    ret = options.inverse(this);
+  }
+
+  return ret;
+});
