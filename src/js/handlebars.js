8bfb58763e89bc12489dc3aebaf35f9da7b73e2d
James Tapsell <email@jrtapsell.co.uk>
2017-04-14 01:19:47 +0100

    Started working on user icons

--- src/js/handlebars.js
+++ src/js/handlebars.js
@@ -1,9 +1,9 @@
 var handlebars  = require('handlebars');
-
+/*
 handlebars.registerHelper( "setTitle", function ( title ){
   $("title").text(title);
   $("#title").text(title);
   $(".title").text(title);
 });
-
+*/
 module.exports = handlebars;
--- src/js/handlebars_client.js
+++ src/js/handlebars_client.js
@@ -3,3 +3,7 @@ Handlebars.registerHelper( "setTitle", function ( title ){
   $("#title").text(title);
   $(".title").text(title);
 });
+
+Handlebars.registerHelper("userIcon", function (userId) {
+  return "<div>" + userId + "</div>";
+});
