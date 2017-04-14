65ce17c3d11e42d4831a62f1bc3bbdb316a70f5e
James Tapsell <email@jrtapsell.co.uk>
2017-04-14 15:04:57 +0100

    Fixed double equals issue

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,6 @@
 function all_defined() {
   for (var index = 0; index < arguments.length; index++) {
-    if (arguments[index] == undefined) {
+    if (arguments[index] === undefined) {
       return false;
     }
   }
