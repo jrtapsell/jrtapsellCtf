6f0c956a7a9ba36c29187596a8276b7ae46bf55e
James Tapsell <email@jrtapsell.co.uk>
2017-04-15 11:08:47 +0100

    Fixed fb being inaccessible

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,63 +1,61 @@
 'use strict';
+var fb;
+
 (function (firebase) {
-var config = {
-  apiKey: "AIzaSyD-b_XD6-Eoe-hQnSsIyHt_s2P2bexLu_E",
-  authDomain: "jrtapsell-ctf.firebaseapp.com",
-  databaseURL: "https://jrtapsell-ctf.firebaseio.com",
-  projectId: "jrtapsell-ctf",
-  storageBucket: "jrtapsell-ctf.appspot.com",
-  messagingSenderId: "706159874560"
-};
+  var config = {
+    apiKey: "AIzaSyD-b_XD6-Eoe-hQnSsIyHt_s2P2bexLu_E",
+    authDomain: "jrtapsell-ctf.firebaseapp.com",
+    databaseURL: "https://jrtapsell-ctf.firebaseio.com",
+    projectId: "jrtapsell-ctf",
+    storageBucket: "jrtapsell-ctf.appspot.com",
+    messagingSenderId: "706159874560"
+  };
 
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
 
-var database = firebase.database();
-var auth = firebase.auth();
+  fb = {
+    "db": firebase.database(),
+    "auth": firebase.auth(),
+    "now": firebase.database.ServerValue.TIMESTAMP,
+    "path": function () {
+      var ret = firebase.database().ref("/");
+      for (var i = 0; i < arguments.length; i++) {
+        ret = ret.child(arguments[i])
+      }
+      return ret;
+    },
+    "authUpdate": function () {
+      firebase.auth().onAuthStateChanged(arguments);
+    },
+    "google": new firebase.auth.GoogleAuthProvider(),
+    "github": new firebase.auth.GithubAuthProvider(),
+    "popupLogin": firebase.auth().signInWithPopup,
+    "logout": firebase.auth().signOut(),
+    "user": undefined
+  };
 
-var fb = {
-  "db": database,
-  "auth": auth,
-  "now": firebase.database.ServerValue.TIMESTAMP,
-  "path": function () {
-    var ret = database.ref("/");
-    for (var i = 0; i < arguments.length; i++) {
-      ret = ret.child(arguments[i])
-    }
-    return ret;
-  },
-  "authUpdate": function() {
-    firebase.auth().onAuthStateChanged(arguments);
-  },
-  "google": new firebase.auth.GoogleAuthProvider(),
-  "github": new firebase.auth.GithubAuthProvider(),
-  "popupLogin": auth.signInWithPopup,
-  "logout": auth.signOut(),
-  "user": 0
-};
-
-fb.authUpdate(function (user) {
-  fb.user = user;
-});
-
-$(function() {
   fb.authUpdate(function (user) {
-    if (user) {
-      $("#login-status").html("Hello, " + user.displayName + ", <a href='/logout/'>Logout</a>");
-      $(".mdl-layout__drawer-button").show();
-      console.log(user);
-      fb.path("users", user.uid).set({
-        image: user.photoURL,
-        name: user.displayName,
-        uid: user.uid,
-        email: user.email,
-        provider: user.providerData[0].providerId
-      });
-    } else {
-      $("#login-status").text("I don't know you");
-      $(".mdl-layout__drawer-button").hide();
-    }
+    fb.user = user;
   });
-});
+
+  $(function () {
+    fb.authUpdate(function (user) {
+      if (user) {
+        $("#login-status").html("Hello, " + user.displayName + ", <a href='/logout/'>Logout</a>");
+        $(".mdl-layout__drawer-button").show();
+        console.log(user);
+        fb.path("users", user.uid).set({
+          image: user.photoURL,
+          name: user.displayName,
+          uid: user.uid,
+          email: user.email,
+          provider: user.providerData[0].providerId
+        });
+      } else {
+        $("#login-status").text("I don't know you");
+        $(".mdl-layout__drawer-button").hide();
+      }
+    });
+  });
 })(firebase);
-firebase = undefined;
