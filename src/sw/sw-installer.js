bcb25098141ab3b987a195559d276db56912a9ea
James Tapsell <email@jrtapsell.co.uk>
2017-04-15 14:55:34 +0100

    Fixed service worker path

--- src/sw/sw-installer.js
+++ src/sw/sw-installer.js
@@ -3,7 +3,7 @@ function log(color, messageContents, data) {
   console.colourLog("#0F0", color, messageContents, data);
 }
 
-navigator.serviceWorker.register('/sw.js').then(function(registration) {
+navigator.serviceWorker.register('/sw/sw.js').then(function(registration) {
   // Registration was successful
   log('#F00', 'ServiceWorker registration successful',registration.scope);
 }).catch(function(err) {
