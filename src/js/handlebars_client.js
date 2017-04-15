aee57beba014b67261fe6f981e1a8384251b240e
James Tapsell <email@jrtapsell.co.uk>
2017-04-15 13:52:09 +0100

    Fixed styling issues

--- src/js/handlebars_client.js
+++ src/js/handlebars_client.js
@@ -6,7 +6,7 @@ Handlebars.registerHelper( "setTitle", function ( title ){
 
 Handlebars.registerHelper("userIcon", function (userId) {
   const escaped_id = Handlebars.Utils.escapeExpression(userId);
-  return new Handlebars.SafeString("<div class='user-icon' data-id='" + userId + "'></div>");
+  return new Handlebars.SafeString("<div class='user-icon square-icon' data-id='" + userId + "'></div>");
 });
 
 Handlebars.registerHelper("reverseEach", function (context) {
