9e24b140c27b9b5c13078e9a6bd32fba19f00580
James Tapsell <email@jrtapsell.co.uk>
2017-04-18 23:34:24 +0100

    Added missing var

--- src/js/redirect.ts
+++ src/js/redirect.ts
@@ -224,7 +224,7 @@ class Router {
     var filesData;
     var messagesData;
 
-    renderUI = () => {
+    var renderUI = () => {
       var currentUserId = fb.user.uid;
       if (all_defined(challengeData, membersData, filesData, messagesData)) {
         this.redirect("challenge", {
